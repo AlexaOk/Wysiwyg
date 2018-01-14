2ef1b4ffad478320f1c50dd82e50f34eb202e504
Alexa <alexa.okito@epitech.eu>
2018-01-14 17:39:59 +0100

    third push

--- jquery.my_wysiwyg.js
+++ jquery.my_wysiwyg.js
@@ -54,119 +54,108 @@
       };
 
       if ($.inArray("police", parameters.buttons) != -1) {
-      wrap.find(".buttons").append("<select class='police'></select>");
-      wrap.find(".police").append("<option value=''>Police</option>");
-      wrap.find(".police").append("<option value='Arial'>Arial</option>");
-      wrap.find(".police").append("<option value='Calibri'>Calibri</option>");
-      wrap.find(".police").append("<option value='Courier'>Courier</option>");
-      wrap.find(".police").append("<option value='Times New Roman'>Times New Roman</option>");
-      wrap.find(".police").change(function() {
-        document.execCommand('fontName', false, $(this).val());
-      });
-    };
+        wrap.find(".buttons").append("<select class='police'></select>");
+        wrap.find(".police").append("<option value=''>Police</option>");
+        wrap.find(".police").append("<option value='Arial'>Arial</option>");
+        wrap.find(".police").append("<option value='Calibri'>Calibri</option>");
+        wrap.find(".police").append("<option value='Courier'>Courier</option>");
+        wrap.find(".police").append("<option value='Times New Roman'>Times New Roman</option>");
+        wrap.find(".police").change(function() {
+          document.execCommand('fontName', false, $(this).val());
+        });
+      };
 
-    if ($.inArray("couleur", parameters.buttons) != -1) {
-      wrap.find(".buttons").append("<select class='color'></select>");
-      wrap.find(".color").append("<option value=''>Couleur</option>");
-      wrap.find(".color").append("<option value='#0000FF'>Bleu</option>");
-      wrap.find(".color").append("<option value='#FF00FF'>Fuschia</option>");
-      wrap.find(".color").append("<option value='#FF0000'>Rouge</option>");
-      wrap.find(".color").append("<option value='#228B22'>Vert</option>");
-      wrap.find(".color").append("<option value='#FF7F50'>Orange</option>");
-      wrap.find(".color").change(function() {
-        document.execCommand('foreColor', false, $(this).val());
-      });
-    };
+      if ($.inArray("couleur", parameters.buttons) != -1) {
+        wrap.find(".buttons").append("<select class='color'></select>");
+        wrap.find(".color").append("<option value=''>Couleur</option>");
+        wrap.find(".color").append("<option value='#0000FF'>Bleu</option>");
+        wrap.find(".color").append("<option value='#FF00FF'>Fuschia</option>");
+        wrap.find(".color").append("<option value='#FF0000'>Rouge</option>");
+        wrap.find(".color").append("<option value='#228B22'>Vert</option>");
+        wrap.find(".color").append("<option value='#FF7F50'>Orange</option>");
+        wrap.find(".color").change(function() {
+          document.execCommand('foreColor', false, $(this).val());
+        });
+      };
 
-    if ($.inArray("lien", parameters.buttons) != -1) {
-      wrap.find(".buttons").append("<button class='lien'><img src='misc/link.png'/></button>");
-      wrap.find(".lien").click(function() {
-        var linkURL = prompt('Enter a URL:', 'http://');
-        document.execCommand('createLink', false, linkURL);
-      });
-    };
+      if ($.inArray("lien", parameters.buttons) != -1) {
+        wrap.find(".buttons").append("<button class='lien'><img src='misc/link.png'/></button>");
+        wrap.find(".lien").click(function() {
+          var linkURL = prompt('Enter a URL:', 'http://');
+          document.execCommand('createLink', false, linkURL);
+        });
+      };
 
-    var size = 3;
+      var size = 3;
 
-    if ($.inArray("plus", parameters.buttons) != -1) {
-      wrap.find(".buttons").append("<button class='plus'><img src='misc/plus.png'/></button>");
-      wrap.find(".plus").click(function() {
-        size = size+1;
-        document.execCommand('fontSize', false, size);
-      });
-    };
+      if ($.inArray("plus", parameters.buttons) != -1) {
+        wrap.find(".buttons").append("<button class='plus'><img src='misc/plus.png'/></button>");
+        wrap.find(".plus").click(function() {
+          size = size+1;
+          document.execCommand('fontSize', false, size);
+        });
+      };
 
-    if ($.inArray("minus", parameters.buttons) != -1) {
-      wrap.find(".buttons").append("<button class='minus'><img src='misc/minus.png'/></button>");
-      wrap.find(".minus").click(function() {
-        size = size-1;
-      document.execCommand('fontSize', false, size);
-      });
-    };
+      if ($.inArray("minus", parameters.buttons) != -1) {
+        wrap.find(".buttons").append("<button class='minus'><img src='misc/minus.png'/></button>");
+        wrap.find(".minus").click(function() {
+          size = size-1;
+          document.execCommand('fontSize', false, size);
+        });
+      };
 
-    if ($.inArray("gauche", parameters.buttons) != -1) {
-      wrap.find(".buttons").append("<button class='gauche'><img src='misc/left.png'/></button>");
-      wrap.find(".gauche").click(function() {
-        document.execCommand('justifyLeft', false, null);
-      });
-    };
+      if ($.inArray("gauche", parameters.buttons) != -1) {
+        wrap.find(".buttons").append("<button class='gauche'><img src='misc/left.png'/></button>");
+        wrap.find(".gauche").click(function() {
+          document.execCommand('justifyLeft', false, null);
+        });
+      };
 
-    if ($.inArray("droite", parameters.buttons) != -1) {
-      wrap.find(".buttons").append("<button class='droite'><img src='misc/right.png'/></button>");
-      wrap.find(".droite").click(function() {
-        document.execCommand('justifyRight', false, null);
-      });
-    };
+      if ($.inArray("droite", parameters.buttons) != -1) {
+        wrap.find(".buttons").append("<button class='droite'><img src='misc/right.png'/></button>");
+        wrap.find(".droite").click(function() {
+          document.execCommand('justifyRight', false, null);
+        });
+      };
 
-    if ($.inArray("centré", parameters.buttons) != -1) {
-      wrap.find(".buttons").append("<button class='centré'><img src='misc/center.png'/></button>");
-      wrap.find(".centré").click(function() {
-        document.execCommand('justifyCenter', false, null);
-      });
-    };
+      if ($.inArray("centré", parameters.buttons) != -1) {
+        wrap.find(".buttons").append("<button class='centré'><img src='misc/center.png'/></button>");
+        wrap.find(".centré").click(function() {
+          document.execCommand('justifyCenter', false, null);
+        });
+      };
 
-    if ($.inArray("justifié", parameters.buttons) != -1) {
-				wrap.find(".buttons").append("<button class='justifié'><img src='misc/justify.png'/></button>");
-				wrap.find(".justifié").click(function() {
-					document.execCommand('justifyFull', false, null);
-				});
-			};
+      if ($.inArray("justifié", parameters.buttons) != -1) {
+        wrap.find(".buttons").append("<button class='justifié'><img src='misc/justify.png'/></button>");
+        wrap.find(".justifié").click(function() {
+          document.execCommand('justifyFull', false, null);
+        });
+      };
 
       if ($.inArray("générer", parameters.buttons) != -1) {
-				wrap.find(".buttons").append("<button class='générer'><img src='misc/html.png'/></button>");
-				wrap.find(".générer").click(function() {
-					var html = wrap.find(".editor").html();
-
-          var myRegex = new RegExp("<br>", "gm");
-
-          var html = html.replace(myRegex, "\n");
-          var str = html.split("\n");
-
-console.log(str);
-
-var content = "";
+        wrap.find(".buttons").append("<button class='générer'><img src='misc/html.png'/></button>");
+        wrap.find(".générer").click(function() {
+          var html = wrap.find(".editor").html();
+          var str = html.split("<br>");
+          var content = "";
           $.each(str, function( index, value ) {
-
             var str1 = "<p> " + value + "</p>";
-             content += str1;
-            console.log(content);
+            content += str1;
             var n = wrap.find("textarea").html(content);
-
           });
-
-
-
           wrap.find("textarea").toggle();
           wrap.find(".editor").toggle();
-				});
-			};
+        });
+      };
 
       if ($.inArray("enregistrer", parameters.buttons) != -1) {
-  				wrap.find(".buttons").append("<button class='enregistrer'><img src='misc/justify.png'/></button>");
-  				wrap.find(".enregistrer").click(function() {
-  					document.execCommand('save', false, null);
-  				});
-  			};
+        wrap.find(".buttons").append("<button class='enregistrer'><img src='misc/justify.png'/></button>");
+        wrap.find(".enregistrer").click(function() {
+          var text = $(".editor").html();
+
+          localStorage.setItem('tex2', text);
+        });
+      };
 
 
     });
--- my_wysiwyg.php
+++ my_wysiwyg.php
@@ -10,18 +10,15 @@
 <body>
 	<h1>My Wysiwyg</h1>
 
-		<textarea style="display:none;"></textarea>
-		<div class="editor" contenteditable="true"></div>
+	<textarea style="display:none;"></textarea>
+	<div class="editor" contenteditable="true"></div>
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 	<script type="text/javascript" src="jquery.my_wysiwyg.js"></script>
-	<script> //Appel du plugin
-		// $(document).ready(function() {
-		// 	$("textarea").my_wysiwyg();
-		// });
-		 $(document).ready(function() {
+	<script>
+	$(document).ready(function() {
 		$("textarea").my_wysiwyg (
-)
-});
+		)
+	});
 	</script>
-	</body>
+</body>
 </html>
